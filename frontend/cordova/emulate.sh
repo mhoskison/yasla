4db587dcfb023f87fb80128d0d7b042f9faca38d
Matt <matth@hoskison.co.uk>
2018-02-12 08:05:35 +0000

    Updated emulator build

--- frontend/cordova/emulate.sh
+++ frontend/cordova/emulate.sh
@@ -4,13 +4,11 @@
 rm -rf www
 
 # Go into the wepapp directory and do a production build
-cd ../webapp/ && gulp build-dev && cd ../cordova
+#cd ../webapp/ &&  ./node_modules/gulp/bin/gulp.js build-dev && cd ../cordova
 
 # Copy the webapp source, but delete our copy of node_modules since that confuses Gradle immensely.
-cp -r ../webapp/ www
-rm -rf www/node_modules
-rm -rf www/yasla.apk
+cp -r ../webapp/app www
 
 # DoIt (tm)
 cordova emulate android
-cp platforms/android/app/build/outputs/apk/debug/app-debug.apk ../webapp/yasla.apk
+#cp platforms/android/app/build/outputs/apk/debug/app-debug.apk ../webapp/yasla.apk
